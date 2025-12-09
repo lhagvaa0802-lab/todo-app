bad275f847b4786beb5cacccc74caa1b1812bc46
lhagvaa0802-lab <lhagvaa0802@gmail.com>
2025-12-09 11:56:49 +0800

    add

--- script.js
+++ script.js
@@ -1,67 +1,87 @@
 const inputBox = document.querySelector(".input-group input");
 const addBtn = document.querySelector(".input-group button");
 const taskList = document.querySelector("#task-list");
-const checkBoxes = document.querySelector(".checkbox");
+const taskDelete = document.getElementById("task-delete");
+const emptyMessage = document.getElementById("emptyMessage");
 
+let tasks = [];
 
+let tasId = 1;
 
-const tasks =[]
+const add = () => {
+  const taskText = inputBox.value;
+  const task = {
+    id: tasId,
+    text: taskText,
+    isCompleted: false,
+  };
+  tasks.push(task);
+  tasId++;
+  clearText();
+  clearInput();
+  renderTasks(tasks);
+};
 
-let tasId =1
+const renderTasks = (taskArr) => {
+  let taskItems = "";
 
-const add =()=>{
-    const taskText = inputBox.value
-    const task ={
-        id: tasId,
-        text: taskText,
-        isCompleted: false}
-    tasks.push(task)
-    tasId++
-    clearInput()
-    renderTasks()
-    
-   
-}
-const renderTasks =()=>{
- let taskItems = ""
- tasks.forEach((task)=> {
-    const taskItem = createTaskItem(task)
-    taskItems += taskItem
-    });
-    
-    taskList.innerHTML = taskItems
+  taskArr.forEach((task) => {
+    const taskItem = createTaskItem(task);
+    taskItems += taskItem;
+  });
 
-}
+  taskList.innerHTML = taskItems;
+};
 
-const createTaskItem =(task)=>{
-    return `
+const createTaskItem = (task) => {
+  return `
         <div class="task-list-item">
            <div class="task-item-left">
-             <input type="checkbox" class="checkbox" ${task.isCompleted} && "checked" /> 
-            <p class="task-text">${task.text}</p>
+             <input type="checkbox" id="${task.id}" ${
+    task.isCompleted && "checked"
+  } onchange="updateTask(${task.id})"/> 
+            <p class="task-text ${task.isCompleted && "completed"}">${
+    task.text
+  }</p>
             </div>
-            <button class="task-delete">Delete</button>
-        </div>
-    `;
-}
-const clearInput =()=>{
-    inputBox.value =""
-}
+            <button id="task-delete" class='delete_btn ${
+              task.isCompleted && "show_delete"
+            }' onclick="deleteBtn(${task.id})">Delete</button>
+        </div>`;
+};
 
+const updateTask = (id) => {
+  const updatedTasks = tasks.map((task) => {
+    if (task.id === id) {
+      return { ...task, isCompleted: !task.isCompleted };
+    } else {
+      return task;
+    }
+  });
 
-checkBoxes.forEach((checkbox)=>{
-    checkbox.addEventListener("change",mychecked);
-})
+  tasks = updatedTasks;
 
+  renderTasks(updatedTasks);
+};
 
-    
+const clearInput = () => {
+  inputBox.value = "";
+};
+let clearText = () => {
+  emptyMessage.innerHTML = "";
+};
+const deleteBtn = (taskId) => {
+  const updatedTasks = tasks.filter((task) => {
+    if (task.id === taskId) {
+      return false;
+    } else {
+      return true;
+    }
+  });
 
+  tasks = updatedTasks;
 
+  renderTasks(updatedTasks);
+};
 
-
-
-addBtn.addEventListener("click", add)
-
-
-
-
+addBtn.addEventListener("click", add);
